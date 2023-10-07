ffe1f85655c5764f5a068ac5295cc334e3eac4e2
JoaoNeto4 <joaoquatlo@hotmail.com>
2023-10-06 22:15:54 -0300

    Network images

--- lib/exemple_images_and_assets.dart
+++ lib/exemple_images_and_assets.dart
@@ -7,21 +7,22 @@ void main() {
 class MyApp extends StatefulWidget {
   final String nome;
 
-  const MyApp({Key? key, this.nome=''}) : super(key: key);
+  const MyApp({Key? key, this.nome = ''}) : super(key: key);
 
   @override
   _MyAppstate createState() => _MyAppstate();
 }
 
 class _MyAppstate extends State<MyApp> {
-
+  
   @override
   Widget build(BuildContext context) {
-     return MaterialApp(
-        home: Scaffold(
-          appBar: AppBar( 
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
             title: Text('Imagens'),
           ),
+          /* For image assets
           body: Image.asset('assets/image/dog.jpg', 
             //width: 200,
             //height: 150,  
@@ -29,8 +30,11 @@ class _MyAppstate extends State<MyApp> {
             width: double.infinity, //for fullscreen
             height: double.infinity, //for fullscreen
           ),
-        ),
-      );
-  }
+          */
 
+          // for network image
+          body: Image.network(
+              'https://t0.gstatic.com/licensed-image?q=tbn:ANd9GcQkrjYxSfSHeCEA7hkPy8e2JphDsfFHZVKqx-3t37E4XKr-AT7DML8IwtwY0TnZsUcQ')),
+    );
+  }
 }

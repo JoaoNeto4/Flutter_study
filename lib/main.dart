d52146647e2ea67b9973bf17035cd1bd81ad3cd1
JoaoNeto4 <joaoquatlo@hotmail.com>
2023-10-11 22:37:25 -0300

    init calculator

--- lib/main.dart
+++ lib/main.dart
@@ -12,35 +12,77 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppstate extends State<MyApp> {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Exemplo scafold"),
+          title: Center(
+            child: Text('Calculadora'),
+          ),
         ),
-        body: Row( //collumn
-          mainAxisAlignment: MainAxisAlignment.center, //alinhamento
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            Center(
-              child: Text('Meu applicativo', 
-                style: TextStyle(fontSize: 20),
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text('número',style: TextStyle(fontSize: 46),),
+              ],
             ),
-            Center(
-              child: Text('linha 2', 
-                style: TextStyle(fontSize: 20),
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text('AC',style: TextStyle(fontSize: 36),),
+                Text(''),
+                Text(''),
+                Text('<x',style: TextStyle(fontSize: 36),),
+              ],
             ),
-            Center(
-              child: Text('linha 3', 
-                style: TextStyle(fontSize: 20),
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text('7',style: TextStyle(fontSize: 36),),
+                Text('8',style: TextStyle(fontSize: 36),),
+                Text('9',style: TextStyle(fontSize: 36),),
+                Text('/',style: TextStyle(fontSize: 36),),
+              ],
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text('4',style: TextStyle(fontSize: 36),),
+                Text('5',style: TextStyle(fontSize: 36),),
+                Text('6',style: TextStyle(fontSize: 36),),
+                Text('x',style: TextStyle(fontSize: 36),),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text('1',style: TextStyle(fontSize: 36),),
+                Text('2',style: TextStyle(fontSize: 36),),
+                Text('3', style: TextStyle(fontSize: 36),),
+                Text('-',style: TextStyle(fontSize: 36),),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text('0',style: TextStyle(fontSize: 36),),
+                Text(',',style: TextStyle(fontSize: 36),),
+                Text('=',style: TextStyle(fontSize: 36),),
+                Text('+',style: TextStyle(fontSize: 36),),
+              ],
+            ),
+            Text('col 3',style: TextStyle(fontSize: 36),),
+            Text('col 4',style: TextStyle(fontSize: 36),),
+            Text('col 5',style: TextStyle(fontSize: 36),),
+            Text('col 6',style: TextStyle(fontSize: 36),),
           ],
         ),
-      ));
+      ),
+    );
   }
-
-} 
+}
+ 

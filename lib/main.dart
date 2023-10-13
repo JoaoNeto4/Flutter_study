dd15cc7a4b50f443edd265f944979f62ca71e4af
JoaoNeto4 <joaoquatlo@hotmail.com>
2023-10-13 19:40:35 -0300

    study setState()

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,6 +14,128 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppstate extends State<MyApp> {
+
+  String numero = '0';
+  double primeiroNumero = 0.0;
+  String operacao = '';
+
+  void calcular(String tecla){
+    switch(tecla){
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+        setState(() {
+          numero += tecla;
+          print('$numero');
+        });
+        break;
+
+      case '8':
+      case '9':
+
+      case ',':
+        setState(() {
+          numero += tecla;
+
+          numero = numero.replaceAll(',', '.');
+
+          if(numero.contains('.')){
+            //double numeroDouble = double.parse(numero);
+            //numero = numeroDouble.toString();
+          } else {
+            int numeroInt = int.parse(numero);
+            numero = numeroInt.toString();
+          }
+          numero = numero.replaceAll('.', ',');
+        });
+        break;
+
+      case '+':
+        operacao = tecla;
+        numero = numero.replaceAll(",", '.');
+        primeiroNumero = double.parse(numero);
+        
+        numero = numero.replaceAll(".", ',');
+        numero = '0';
+        break;
+      
+      case '=':
+        double resultado = 0.0;
+
+        if(operacao == '/'){
+          if(double.parse(numero) * 1 == 0){
+            print('ERRO: divição por 0');
+            return;
+          }
+        }
+
+        if(operacao == '+'){
+          resultado = primeiroNumero + double.parse(numero);
+        }
+
+        if(operacao == '-'){
+          resultado = primeiroNumero - double.parse(numero);
+        }
+
+        if(operacao == 'x'){
+          resultado = primeiroNumero * double.parse(numero);
+        }
+
+        if(operacao == '/'){
+          resultado = primeiroNumero / double.parse(numero);
+        }
+
+        String resultadoString = resultado.toString();
+
+        List<String> resultadoPartes = resultadoString.split('.');
+
+        if(int.parse(resultadoPartes[1]) * 1 == 0){
+          setState(() {
+            numero = int.parse(resultadoPartes[0]).toString();
+          });
+
+        }else{
+          //double resultadoFormatado = resultado;
+          setState(() {
+            numero = resultado.toString();
+          });
+        }
+        break;
+
+      case '/':
+        operacao = tecla;
+        numero = numero.replaceAll(',', '.');
+        primeiroNumero = double.parse(numero);
+        numero = numero.replaceAll('.', ',');
+        numero = '0';
+        break;
+
+      case '<x':
+        setState(() {
+          if(numero.length > 0){
+            numero = numero.substring(0, numero.length -1);
+          }
+        });
+        break;
+
+      case 'AC':
+        setState(() {
+          numero = '0';
+        });
+        break;
+
+      default:
+        numero += tecla;
+        break;
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,57 +151,110 @@ class _MyAppstate extends State<MyApp> {
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                Text('número',style: TextStyle(fontSize: 46),),
+                Text(numero, style: TextStyle(fontSize: 46),),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Text('AC',style: TextStyle(fontSize: 36),),
+                GestureDetector(
+                  onTap: () => calcular('AC'),
+                  child: Text('AC',style: TextStyle(fontSize: 36),),
+                ),
                 Text(''),
                 Text(''),
-                Text('<x',style: TextStyle(fontSize: 36),),
+                GestureDetector(
+                  onTap: () => calcular('<x'),
+                  child: Text('<x',style: TextStyle(fontSize: 36),),
+                ),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Text('7',style: TextStyle(fontSize: 36),),
-                Text('8',style: TextStyle(fontSize: 36),),
-                Text('9',style: TextStyle(fontSize: 36),),
-                Text('/',style: TextStyle(fontSize: 36),),
+                GestureDetector(
+                  onTap: () {print('clicou');
+                    calcular('7');
+                  },
+                  child: Text('7',style: TextStyle(fontSize: 36),),
+                ),
+                GestureDetector(
+                  onTap: () => calcular('8'),
+                  child: Text('8',style: TextStyle(fontSize: 36),),
+                ),
+                GestureDetector(
+                  onTap: () => calcular('9'),
+                  child: Text('9',style: TextStyle(fontSize: 36),),
+                ),
+                GestureDetector(
+                  onTap: () => calcular('/'),
+                  child: Text('/',style: TextStyle(fontSize: 36),),
+                ),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Text('4',style: TextStyle(fontSize: 36),),
-                Text('5',style: TextStyle(fontSize: 36),),
-                Text('6',style: TextStyle(fontSize: 36),),
-                Text('x',style: TextStyle(fontSize: 36),),
+                GestureDetector(
+                  onTap: () => calcular('4'),
+                  child: Text('4',style: TextStyle(fontSize: 36),),
+                ),
+                GestureDetector(
+                  onTap: () => calcular('5'),
+                  child: Text('5',style: TextStyle(fontSize: 36),),
+                ),
+                GestureDetector(
+                  onTap: () => calcular('6'),
+                  child: Text('6',style: TextStyle(fontSize: 36),),
+                ),
+                GestureDetector(
+                  onTap: () => calcular('x'),
+                  child: Text('x',style: TextStyle(fontSize: 36),),
+                ),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Text('1',style: TextStyle(fontSize: 36),),
-                Text('2',style: TextStyle(fontSize: 36),),
-                Text('3', style: TextStyle(fontSize: 36),),
-                Text('-',style: TextStyle(fontSize: 36),),
+                GestureDetector(
+                  onTap: () => calcular('1'),
+                  child: Text('1',style: TextStyle(fontSize: 36),),
+                ),
+                GestureDetector(
+                  onTap: () => calcular('2'),
+                  child: Text('2',style: TextStyle(fontSize: 36),),
+                ),
+                GestureDetector(
+                  onTap: () => calcular('3'),
+                  child: Text('3',style: TextStyle(fontSize: 36),),
+                ),
+                GestureDetector(
+                  onTap: () => calcular('-'),
+                  child: Text('-',style: TextStyle(fontSize: 36),),
+                ),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Text('0',style: TextStyle(fontSize: 36),),
-                Text(',',style: TextStyle(fontSize: 36),),
-                Text('=',style: TextStyle(fontSize: 36),),
-                Text('+',style: TextStyle(fontSize: 36),),
+                GestureDetector(
+                  onTap: () => calcular('0'),
+                  child: Text('0',style: TextStyle(fontSize: 36),),
+                ),
+                GestureDetector(
+                  onTap: () => calcular(','),
+                  child: Text(',',style: TextStyle(fontSize: 36),),
+                ),
+                GestureDetector(
+                  onTap: () => calcular('='),
+                  child: Text('=',style: TextStyle(fontSize: 36),),
+                ),
+                GestureDetector(
+                  onTap: () => calcular('+'),
+                  child: Text('+',style: TextStyle(fontSize: 36),),
+                ),
               ],
             ),
-            Text('col 3',style: TextStyle(fontSize: 36),),
-            Text('col 4',style: TextStyle(fontSize: 36),),
-            Text('col 5',style: TextStyle(fontSize: 36),),
             Text('col 6',style: TextStyle(fontSize: 36),),
           ],
         ),
